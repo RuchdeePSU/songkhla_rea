2ea5f6b8296b7470385ca4cc82754795b3e757ac
Ruchdee Binmad <ruchdee.b@psu.ac.th>
2017-05-16 18:56:55 +0700

    coding property-types-update.php

---
+++ property-types-update.php
@@ -0,0 +1,256 @@
+<?php
+    session_start();
+
+    if (!isset($_SESSION['email'])) {
+        header("Location: sign-in.php");
+    }
+
+    // check property type id
+    if (isset($_GET['type_id'])) {
+      include_once 'assets/php/dbconnect.php';
+      include_once 'assets/php/property_type.php';
+
+      // get connection
+      $database = new Database();
+      $db = $database->getConnection();
+
+      // pass connection to property_types table
+      $property_type = new Property_type($db);
+      $property_type->prop_type_id = $_GET['type_id'];
+      $result_type = $property_type->readone();
+      $row_type = mysqli_fetch_array($result_type);
+
+      // form is submitted
+      if (isset($_POST['property-type-submit'])) {
+          $property_type->prop_type_desc = $_POST['property-type-desc'];
+          $property_type->prop_type_status = $_POST['property-type-status'];
+
+          // update
+          if ($property_type->update()) {
+              header("Location: property-types-listing.php");
+          } else {
+              $success = false;
+          }
+      }
+    }
+
+?>
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="ThemeStarz">
+
+    <link href='http://fonts.googleapis.com/css?family=Roboto:300,400,700' rel='stylesheet' type='text/css'>
+    <link href="assets/fonts/font-awesome.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.css" type="text/css">
+    <link rel="stylesheet" href="assets/css/bootstrap-select.min.css" type="text/css">
+    <link rel="stylesheet" href="assets/css/magnific-popup.css" type="text/css">
+    <link rel="stylesheet" href="assets/css/jquery.slider.min.css" type="text/css">
+    <link rel="stylesheet" href="assets/css/owl.carousel.css" type="text/css">
+    <link rel="stylesheet" href="assets/css/style.css" type="text/css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Pridi:300,400">
+    <style>
+        h1, h2, h3, h4, h5, h6, legend, a, .btn, label, ul { font-family: 'Pridi', serif; }
+    </style>
+
+    <title>โครงการสำรวจอุปทานที่อยู่อาศัยเพื่อจัดแผนที่เบื้องต้น | ข้อมูลประเภทอสังหาริมทรัพย์</title>
+
+</head>
+
+<body class="page-sub-page page-my-properties page-account" id="page-top">
+<!-- Wrapper -->
+<div class="wrapper">
+    <!-- Navigation -->
+    <div class="navigation">
+        <div class="container">
+          <header class="navbar" id="top" role="banner">
+              <div class="navbar-header">
+                  <button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".bs-navbar-collapse">
+                      <span class="sr-only">Toggle navigation</span>
+                      <span class="icon-bar"></span>
+                      <span class="icon-bar"></span>
+                      <span class="icon-bar"></span>
+                  </button>
+                  <div class="navbar-brand nav" id="brand">
+                      <!--
+                      <a href="index-google-map-fullscreen.html"><img src="assets/img/logo.png" alt="brand"></a>
+                    -->
+                      <legend>
+                        โครงการสำรวจอุปทานที่อยู่อาศัยเพื่อจัดแผนที่เบื้องต้น
+                      </legend>
+                  </div>
+              </div>
+              <nav class="collapse navbar-collapse bs-navbar-collapse navbar-right" role="navigation">
+                  <ul class="nav navbar-nav">
+                      <li><a href="index.php">หน้าแรก</a></li>
+                      <li><a href="#">ความเป็นมาของโครงการ</a></li>
+                      <li><a href="#">ติดต่อ</a></li>
+                      <?php
+                          if (isset($_SESSION['user_type']) && $_SESSION['user_type'] == "admin") {
+                            echo "<li class='active'><a href='#'>จัดการข้อมูล</a></li>";
+                          } else {
+                            // menu for typical user
+                          }
+                      ?>
+                      <?php
+                          if (isset($_SESSION['email']) && isset($_SESSION['name'])) {
+                              echo "<li>" . $_SESSION['name'] . "<a href='assets/php/sign-out.php'>[ออกจากระบบ]</a></li>";
+                          } else {
+                              echo "<li><a href='sign-in.php'>ลงชื่อเข้าใช้</a></li>";
+                          }
+                      ?>
+                  </ul>
+              </nav><!-- /.navbar collapse-->
+              <!-- <div class="add-your-property">
+                  <a href="#" class="btn btn-default"><i class="fa fa-plus"></i><span class="text">เพิ่มประเภทอสังหาริมทรัพย์</span></a>
+              </div> -->
+          </header><!-- /.navbar -->
+        </div><!-- /.container -->
+    </div><!-- /.navigation -->
+    <!-- end Navigation -->
+    <!-- Page Content -->
+    <div id="page-content">
+        <!-- Breadcrumb -->
+        <div class="container">
+            <ol class="breadcrumb">
+              <li>หน้าแรก</li>
+              <li>จัดการข้อมูล</li>
+              <li class="active">ข้อมูลประเภทอสังหาริมทรัพย์</li>
+            </ol>
+        </div>
+        <!-- end Breadcrumb -->
+
+        <div class="container">
+            <div class="row">
+            <!-- sidebar -->
+            <div class="col-md-3 col-sm-2">
+                <section id="sidebar">
+                    <header><h3>ประเภทข้อมูล</h3></header>
+                    <aside>
+                        <ul class="sidebar-navigation">
+                          <li><a href="profile.php"><i class="fa fa-user"></i><span>ข้อมูลผู้ใช้งาน</span></a></li>
+                          <li class="active"><a href="property-types-listing.php"><i class="fa fa-list"></i><span>ข้อมูลประเภทอสังหาริมทรัพย์</span></a></li>
+                          <li><a href="property-municipals-listing.php"><i class="fa fa-list"></i><span>ข้อมูลเทศบาล</span></a></li>
+                          <li><a href="properties-listing.php"><i class="fa fa-home"></i><span>ข้อมูลโครงการ</span></a></li>
+                        </ul>
+                    </aside>
+                </section><!-- /#sidebar -->
+            </div><!-- /.col-md-3 -->
+            <!-- end Sidebar -->
+                <!-- My Properties -->
+                <div class="col-md-9 col-sm-10">
+                    <section id="my-properties">
+                        <header><h1>ประเภทอสังหาริมทรัพย์</h1></header>
+                        <div class="my-properties">
+                          <div class="row">
+                            <div class="col-md-6 col-sm-6">
+                              <form role="form" id="property-types" method="post" action="<?php $_SERVER['PHP_SELF'] ?>">
+                                <div class="form-group">
+                                    <label for="property-type-desc">ชื่อเรียกประเภทอสังหาริมทรัพย์</label>
+                                    <input type="text" class="form-control" id="property-type-desc" name="property-type-desc" placeholder="ใส่ชื่อเรียกประเภทอสังหาริมทรัพย์" value="<?php echo $row_type['prop_type_desc']; ?>">
+                                </div><!-- /.form-group -->
+                                <div class="form-group">
+                                    <label for="property-type-status">สถานะการใช้งาน</label>
+                                    <select name="property-type-status" id="property-type-status">
+                                        <option value="1" <?php if ($row_type['prop_type_status']) {
+                                          echo "selected";
+                                        } ?>>ใช้งานปกติ</option>
+                                        <option value="0" <?php if (!$row_type['prop_type_status']) {
+                                          echo "selected";
+                                        } ?>>ยกเลิกการใช้งาน</option>
+                                    </select>
+                                </div><!-- /.form-group -->
+                                <div class="form-group clearfix">
+                                    <button type="submit" class="btn pull-right btn-default" id="property-type-submit" name="property-type-submit">บันทึกข้อมูล</button>
+                                </div><!-- /.form-group -->
+                              </form>
+                              <div class="center-block">
+                                  <?php
+                                    if (isset($success)) {
+                                        if ($success) {
+                                            echo "<div class='alert alert-success text-center'>บันทึกข้อมูลเรียบร้อยแล้ว</div>";
+                                        } else {
+                                            echo "<div class='alert alert-danger text-center'>พบข้อผิดพลาด! ไม่สามารถบันทึกข้อมูลได้</div>";
+                                        }
+                                    }
+                                  ?>
+                              </div>
+                            </div>
+                            <div class="col-md-offset-6 col-sm-offset-6">
+                            </div>
+                          </div>
+
+                        </div><!-- /.my-properties -->
+                    </section><!-- /#my-properties -->
+                </div><!-- /.col-md-9 -->
+                <!-- end My Properties -->
+            </div><!-- /.row -->
+        </div><!-- /.container -->
+    </div>
+    <!-- end Page Content -->
+    <!-- Page Footer -->
+    <footer id="page-footer">
+        <div class="inner">
+            <aside id="footer-main">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-3 col-sm-3">
+                            <!-- <article>
+                                <h3>About Us</h3>
+                                <p>Vel fermentum ipsum. Suspendisse quis molestie odio. Interdum et malesuada fames ac ante ipsum
+                                    primis in faucibus. Quisque aliquet a metus in aliquet. Praesent ut turpis posuere, commodo odio
+                                    id, ornare tortor
+                                </p>
+                                <hr>
+                                <a href="#" class="link-arrow">Read More</a>
+                            </article> -->
+                        </div><!-- /.col-sm-3 -->
+                        <div class="col-md-3 col-sm-3">
+                        </div><!-- /.col-sm-3 -->
+                        <div class="col-md-3 col-sm-3">
+                            <article>
+                                <h3>Contact</h3>
+                            </article>
+                        </div><!-- /.col-sm-3 -->
+                        <div class="col-md-3 col-sm-3">
+                            <article>
+                                <h3>Useful Links</h3>
+                                <ul class="list-unstyled list-links">
+                                    <li><a href="#">All Properties</a></li>
+                                    <li><a href="#">Privacy Policy</a></li>
+                                    <li><a href="#">Login and Register Account</a></li>
+                                    <li><a href="#">FAQ</a></li>
+                                    <li><a href="#">Terms and Conditions</a></li>
+                                </ul>
+                            </article>
+                        </div><!-- /.col-sm-3 -->
+                    </div><!-- /.row -->
+                </div><!-- /.container -->
+            </aside><!-- /#footer-main -->
+            <aside id="footer-thumbnails" class="footer-thumbnails"></aside><!-- /#footer-thumbnails -->
+            <aside id="footer-copyright">
+                <div class="container">
+                    <span>Copyright © 2013. All Rights Reserved.</span>
+                    <span class="pull-right"><a href="#page-top" class="roll">Go to top</a></span>
+                </div>
+            </aside>
+        </div><!-- /.inner -->
+    </footer>
+    <!-- end Page Footer -->
+</div>
+
+<script type="text/javascript" src="assets/js/jquery-2.1.0.min.js"></script>
+<script type="text/javascript" src="assets/js/jquery-migrate-1.2.1.min.js"></script>
+<script type="text/javascript" src="assets/bootstrap/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="assets/js/smoothscroll.js"></script>
+<script type="text/javascript" src="assets/js/bootstrap-select.min.js"></script>
+<script type="text/javascript" src="assets/js/retina-1.1.0.min.js"></script>
+<script type="text/javascript" src="assets/js/custom.js"></script>
+<!--[if gt IE 8]>
+<script type="text/javascript" src="assets/js/ie.js"></script>
+<![endif]-->
+
+</body>
+</html>
