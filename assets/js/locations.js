655821369915d2db3386d4738dfe8bfe4ffcd79f
Ruchdee Binmad <ruchdee.b@psu.ac.th>
2017-05-22 10:58:54 +0700

    commit locations.js

--- assets/js/locations.js
+++ assets/js/locations.js
@@ -1,1 +1,3 @@
 var locations = [
+["ปิยะทรัพย์ บ้านไร่", "88/5 ม.8 ถ.บ้านไร่-ทุ่งขมิ้น ต.บ้านพรุ อ.หาดใหญ่", "฿0-฿0", 6.896111, 100.476111, "properties-detail.php", "assets/img/properties/property-sample.jpg", "assets/img/property-types/single-family.png"]
+];
