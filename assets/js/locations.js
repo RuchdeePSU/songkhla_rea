ed17b667c462834d9d24ac33664c824b143c50a0
Ruchdee <ruchdee.b@psu.ac.th>
2017-05-22 11:06:03 +0700

    commit sql file

--- assets/js/locations.js
+++ assets/js/locations.js
@@ -1,6 +1,13 @@
 var locations = [
-["ฮาร์โมนีวิลล์ 1", "ต.บ้านพรุ อ.หาดใหญ่", "฿3,300,000-฿0", 6.98055556, 100.47527778, "properties-detail.php", "assets/img/properties/property-sample.jpg", "assets/img/property-types/single-family.png", "3"],
-["ฮาร์โมนีวิลล์ 3", "ถ.บ้านโป๊ะหมอ ต.บ้านพรุ อ.หาดใหญ่", "฿3,500,000-฿0", 6.9325, 100.46916667, "properties-detail.php", "assets/img/properties/property-sample.jpg", "assets/img/property-types/single-family.png", "4"],
+["ปิยะทรัพย์ บ้านไร่", "88/5 ม.8 ถ.บ้านไร่-ทุ่งขมิ้น ต.บ้านพรุ ", "฿1,790,000-฿0", 6.896111, 100.476111, "properties-detail.php", "assets/img/properties/property-sample.jpg", "assets/img/property-types/single-family.png", "1"],
+["ฮาร์โมนีบิซ", "ต.บ้านพรุ อ.หาดใหญ่", "฿2,800,000-฿0", 6.97166667, 100.475, "properties-detail.php", "assets/img/properties/property-sample.jpg", "assets/img/property-types/single-family.png", "2"],
+["ฮาร์โมนีวิลล์ 1", "ต.บ้านพรุ อ.หาดใหญ่", "฿1,690,000-฿0", 6.98055556, 100.47527778, "properties-detail.php", "assets/img/properties/property-sample.jpg", "assets/img/property-types/single-family.png", "3"],
+["ฮาร์โมนีวิลล์ 3", "ถ.บ้านโป๊ะหมอ ต.บ้านพรุ อ.หาดใหญ่", "฿2,420,000-฿0", 6.9325, 100.46916667, "properties-detail.php", "assets/img/properties/property-sample.jpg", "assets/img/property-types/single-family.png", "4"],
+["ฉัตรทองพาวิลเลี่ยน 5 เฟส 2", "ถ.เยื้องสนามกีฬาพรุค้างคาว ต.บ้านพรุ ", "฿3,500,000-฿0", 6.91972222, 100.45861111, "properties-detail.php", "assets/img/properties/property-sample.jpg", "assets/img/property-types/single-family.png", "5"],
+["อาเซียน ซิตี้ รีสอร์ท", "ถ.กาญจนวณิชย์ ต.หาดใหญ่ อ.หาดใหญ่", "฿1,900,000-฿0", 6.99472222, 100.48527778, "properties-detail.php", "assets/img/properties/property-sample.jpg", "assets/img/property-types/single-family.png", "6"],
+["เบญจพร คลองหวะ", "ถ.หาดใหญ่-ปัตตานี ต.คอหงส์ อ.หาดใหญ่", "฿2,800,000-฿0", 6.97265833, 100.48944444, "properties-detail.php", "assets/img/properties/property-sample.jpg", "assets/img/property-types/single-family.png", "7"],
+["ปาล์มสปริงส์ 8", "ถ.คลองยาเหนือ 9 ต.บ้านพรุ อ.หาดใหญ่", "฿1,850,000-฿0", 6.94611111, 100.47888889, "properties-detail.php", "assets/img/properties/property-sample.jpg", "assets/img/property-types/single-family.png", "8"],
+["ปาล์มสปริงส์ 9", "ถ.กาญจนวณิชย์ ต.บ้านพรุ อ.หาดใหญ่", "฿3,410,000-฿0", 6.93916667, 100.46861111, "properties-detail.php", "assets/img/properties/property-sample.jpg", "assets/img/property-types/single-family.png", "9"],
 ["ปาล์มสปริงส์ 10", "ถ.พรุค้างคาว ต.บ้านพรุ อ.หาดใหญ่", "฿2,880,000-฿0", 6.91972222, 100.46444444, "properties-detail.php", "assets/img/properties/property-sample.jpg", "assets/img/property-types/single-family.png", "10"],
 
 ];
